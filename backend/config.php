6a40c881e501e716a3707381aae6cacfbaae50e3
Lavika Panwar <153886404+LavikaPanwar@users.noreply.github.com>
2025-10-09 01:01:05 +0530

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -1,1 +1,18 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 
+// XAMPP MySQL connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "liaa-shoppingwebsite"; // database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
